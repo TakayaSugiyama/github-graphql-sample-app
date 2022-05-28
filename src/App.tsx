c72baeb1fe65af24adac4f8f9e221ba8b758e4f6
takayasugiyama <s.takaya1027@gmail.com>
2022-05-28 21:06:59 +0900

    リポジトリの情報を取得できるかどうかを確認した

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { gql , useQuery } from "@apollo/client"
-import { ME } from "./graphql"
+import { useQuery } from "@apollo/client"
+import { ME, searchRepository } from "./graphql"
+
+const VARIABLES = {
+  after: null,
+  before: null,
+  first: 5,
+  last: null,
+  query: "フロントエンドエンジニア"
+}
 
 function App() {
-  const { loading, error, data } = useQuery(ME)
+  const { loading, error, data } = useQuery(searchRepository, { variables: { ...VARIABLES } })
   if (loading) return <p>loading</p>
   if (error) return <p>{ error.message }</p>
+  console.log(data)
   return (
-    <div>{data.user.name}</div>
+    <div></div>
   );
 }
 
--- src/graphql.ts
+++ src/graphql.ts
@@ -7,3 +7,44 @@ export const ME = gql`
     }
   }
 `;
+
+export const searchRepository = gql`
+  query searchRepos(
+    $after: String
+    $before: String
+    $first: Int
+    $last: Int
+    $query: String!
+  ) {
+    search(
+      after: $after
+      before: $before
+      first: $first
+      last: $last
+      query: $query
+      type: REPOSITORY
+    ) {
+      repositoryCount
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      edges {
+        cursor
+        node {
+          ... on Repository {
+            id
+            name
+            url
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+          }
+        }
+      }
+    }
+  }
+`;
